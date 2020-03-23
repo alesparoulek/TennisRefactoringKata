c893162768c3eac1762a6502723b585880a631a9
Ales Paroulek <456555@mail.muni.cz>
2020-03-24 00:01:55 +0100

    added comment

--- Tennis/ITennisGame.cs
+++ Tennis/ITennisGame.cs
@@ -2,7 +2,11 @@ namespace Tennis
 {
     public interface ITennisGame
     {
-        void WonPoint(string playerName);
+        void WonPoint(string playerName); // I don't think this is too good
+        // since there are allways only 2 players (or 2 teams) it should be enough to give the function just two choices.
+        // I would recommend splitting it into two functions like Player1WonPoint() and Player2WonPoint()
+        // so there would be no need to compare strings.
+
         string GetScore();
     }
 }
