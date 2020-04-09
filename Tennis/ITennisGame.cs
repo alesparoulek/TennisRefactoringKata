83c303b63f60485468fe3aa4968e2f27bb8e7c3d
Ales Paroulek <456555@mail.muni.cz>
2020-04-09 19:49:58 +0200

    split WonPoint(playerName) into two functions

--- Tennis/ITennisGame.cs
+++ Tennis/ITennisGame.cs
@@ -2,10 +2,10 @@ namespace Tennis
 {
     public interface ITennisGame
     {
-        void WonPoint(string playerName); // I don't think this is too good
-        // since there are allways only 2 players (or 2 teams) it should be enough to give the function just two choices.
-        // I would recommend splitting it into two functions like Player1WonPoint() and Player2WonPoint()
-        // so there would be no need to compare strings.
+        // since there are allways only 2 players (or 2 teams), using the following two functions
+        // is better than comparing strings in "void WonPoint(string playerName)"
+        void Player1WonPoint();
+        void Player2WonPoint();
 
         string GetScore();
     }
